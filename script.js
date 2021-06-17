14be19cbc6b0251813bb677100e14f48977cf2f3
Aleksandar Milic <alexmilic01@gmail.com>
2021-06-17 21:50:27 +0200

    Sections animations

--- script.js
+++ script.js
@@ -132,6 +132,27 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 });
 headerObserver.observe(header);
 
+
+// Sections animation
+const allSections = document.querySelectorAll('.section');
+const revealSection = function(entries, observer) {
+    const [entry] = entries;
+    if (!entry.isIntersecting) return;
+
+    entry.target.classList.remove('section--hidden');
+    observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+    root: null,
+    threshold: 0.25
+});
+
+allSections.forEach(function(section) {
+    sectionObserver.observe(section);
+    section.classList.add('section--hidden');
+});
+
 // calcualte random color
 // const randomInt = (min, max) => Math.floor(Math.random() * (max - min +1) + min);
 // const randomColor = () => `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
